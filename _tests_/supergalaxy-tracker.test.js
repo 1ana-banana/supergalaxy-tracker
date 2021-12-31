8d545a414af2b65e870f702a253d91c4b0b51eb8
Anastasia Han <anastasia.han@yahoo.com>
2021-12-31 15:17:42 -0500

    Passes test for mercuryAge

--- _tests_/supergalaxy-tracker.test.js
+++ _tests_/supergalaxy-tracker.test.js
@@ -3,11 +3,15 @@ import AgeCalculator from './../src/js/supergalaxy-tracker';
 describe('AgeCalculator', () => {
   let myAgeCalculator;
   beforeEach(() => {
-    myAgeCalculator = new AgeCalculator(26, 79);
+    myAgeCalculator = new AgeCalculator(27, 79);
   });
 
 
-  test('should correctly create a AgeCalculator object with 2 key value pairs, one for age, one for life expectancy', () => {
-    expect(myAgeCalculator.age).toEqual(26);
+  test('should correctly create a AgeCalculator object with 2 value pairs, one for age, one for life expectancy', () => {
+    expect(myAgeCalculator.age).toEqual(27);
     expect(myAgeCalculator.lifeExpectancyEarth).toEqual(79);   
-  });});
+  });
+  test('should take this.age and divide it by .24 and return that value to result in a new mercuryAge value', () => {    
+    expect(myAgeCalculator.mercuryAge()).toEqual(112.5);
+  });
+});
--- src/js/supergalaxy-tracker.js
+++ src/js/supergalaxy-tracker.js
@@ -1,6 +1,9 @@
-export default class GalaxyCalculator {
+export default class AgeCalculator {
   constructor(userAge, userLifeExpectancy) {
     this.age = userAge;
     this.lifeExpectancyEarth = userLifeExpectancy;
+  }
+  mercuryAge() {
+    return this.age / .24;
   }
 }
